b7ec31a320e9c07401bd11c0ee960e3d9ee85248
emre <emre>
2024-03-16 16:37:23 +0300

    Kosul_Yapilari

---
+++ app.js
@@ -0,0 +1,21 @@
+// Koşul yapıları
+
+// let not = 65;
+// if(not>50) {
+//     console.log("Geçtiniz , notunuz :" + not);
+// }else {
+//     console.log("Kaldınız , notunuz : " + not);
+// }
+
+
+
+
+// let yas = Number (prompt("Yaşınızı giriniz"));
+// let para = Number (prompt("Para miktarını giriniz"));
+
+// if(yas>=18 && para >= 3000) {
+//     alert("Ehliyet almaya hak kazandınız")
+// }else {
+//     alert("Ehliyet almak için uygun kriterlere sahip değilsiniz")
+// }
+
